9801c595583bc1ad1167139cfa4c0deb412cd274
Nuruddin Ashr <nuruddin.ashr@grabtaxi.com>
2022-10-12 14:09:52 +0700

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
@@ -13,13 +14,14 @@ const todoClient = new AxiosTodoClient({
   tokenStorage: localTokenStorage('__reactTodo:token'),
 });
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App todoClient={todoClient} />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
